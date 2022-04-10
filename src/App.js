c5438c8934b4bef90b093e87679f5a65ee4abdf3
bhtibrewal <42600164+bhtibrewal@users.noreply.github.com>
2022-04-10 22:40:06 +0530

    remove unused code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
-import { Navbar, Sidebar } from "./components";
-import Mockman from "mockman-js";
+
 
 function App() {
   return (
     <div className="body">
-      <Navbar />
-      <Sidebar />
+
       
     </div>
   );
